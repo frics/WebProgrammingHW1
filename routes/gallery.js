cc9f2c823411ba96bd59ecbea45f6b272a812fc7
Frics <jimmy9711@naver.com>
2020-12-12 16:48:00 +0900

    tmp save

--- routes/gallery.js
+++ routes/gallery.js
@@ -65,7 +65,7 @@ router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
       );
       await post.addHashtags(result.map(r => r[0]));
     }*/
-    res.redirect('/');
+    res.redirect('/gallery');
   } catch (error) {
     console.error(error);
     next(error);
